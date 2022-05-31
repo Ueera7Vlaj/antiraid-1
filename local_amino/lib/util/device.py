f11638dab7f77e2998a086c42d122767d2c658b3
Ravi3s <97759172+Ravi3s@users.noreply.github.com>
2022-05-31 10:18:17 +0530

    Create device.py

---
+++ local_amino/lib/util/device.py
@@ -0,0 +1,8 @@
+class DeviceGenerator:
+    def __init__(self, deviceId=None):
+        if deviceId:
+            self.device_id = deviceId
+        else:
+            self.device_id = "428575C33C1F7E31CAD54B7B6A2A761DB8434410FADAABFB4AF8BE53C72D91841A948DEF6D9CA6E2E1"
+
+        self.user_agent = "Dalvik/2.1.0 (Linux; U; Android 5.1.1; SM-G973N Build/beyond1qlteue-user 5; com.narvii.amino.master/3.4.33562)"
