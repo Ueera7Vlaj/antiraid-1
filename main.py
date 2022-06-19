8153fed56a50235fee6bc2abe82bf37a73036da0
Bakugo <85675964+bakugo34@users.noreply.github.com>
2022-06-19 19:01:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -67,8 +67,8 @@ sidd=(file.read())
 WARNS=[]
 ANTI_SPAM = {}
 JOIN_LEAVE_DETECTOR ={}
-si=mongo["sid"]
-sit=si["sds"]
+si=mongo["sidd"]
+sit=si["sdsss"]
 ress=sit.find({},{'_id': 0})
 for i in ress:
 	sidss=i["sid"]
@@ -84,8 +84,8 @@ def restarts():
     heroku_conn = heroku3.from_key(key)
     botapp= heroku_conn.apps()[app_name]
     botapp.restart()
-client=BotAmino(email="jeonk333000@gmail.com", password="sksksk")
-#client=BotAmino(sid=sidss)
+#client=BotAmino(email="jeonk333000@gmail.com", password="sksksk")
+client=BotAmino(sid=sidss)
 
 
 
